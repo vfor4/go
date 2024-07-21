004c7f23bcec8029e29278f6943373c3b0bf02d2
vfor4 <trandangvu2708@gmail.com>
2024-07-21 09:57:33 +0700

    best practice for constructing pipeline

--- main.go
+++ main.go
@@ -5,24 +5,55 @@ import (
 )
 
 func main() {
-	values := []int{1, 2, 3, 4}
-	for _, v := range add(mul(values, 2), 1) {
+	done := make(chan interface{})
+	defer close(done)
+	inChan := generator(done, 1, 2, 3, 4)
+	for v := range multiply(done, addv(done, inChan, 1), 2) {
 		fmt.Println(v)
 	}
 }
 
-func mul(arr []int, mulier int) []int {
-	m := make([]int, len(arr))
-	for i, v := range arr {
-		m[i] = v * mulier
-	}
-	return m
+func generator(done <-chan interface{}, values ...int) <-chan int {
+	c := make(chan int, len(values))
+	go func() {
+		defer close(c)
+		for _, v := range values {
+			select {
+			case c <- v:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return c
 }
 
-func add(arr []int, addlier int) []int {
-	r := make([]int, len(arr))
-	for k, v := range arr {
-		r[k] = v + addlier
-	}
-	return r
+func multiply(done <-chan interface{}, intStream <-chan int, multiplier int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		for v := range intStream {
+			select {
+			case c <- v * multiplier:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return c
+}
+
+func addv(done <-chan interface{}, intStream <-chan int, additive int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		for v := range intStream {
+			select {
+			case c <- v + additive:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return c
 }
